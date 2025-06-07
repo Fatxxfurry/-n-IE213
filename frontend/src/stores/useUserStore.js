55d34b610858d63a268ef544b9052f6ea0060fdc
Hung <ares01202975777@gmail.com>
2025-06-07 16:26:31 +0700

    get true user

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -17,7 +17,7 @@ export const useUserStore = create((set, get) => ({
 
     try {
       const res = await axios.post("/auth/signup", { name, email, password });
-      set({ user: res.data, loading: false });
+      set({ user: res.data.user, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.response.data.message || "An error occurred");
@@ -29,7 +29,7 @@ export const useUserStore = create((set, get) => ({
     try {
       const res = await axios.post("/auth/login", { email, password });
 
-      set({ user: res.data, loading: false });
+      set({ user: res.data.user, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.response.data.message || "An error occurred");
@@ -48,10 +48,10 @@ export const useUserStore = create((set, get) => ({
   },
 
   checkAuth: async () => {
-    set({ checkingAuth: true });
+    set({ checkingAuth: true });  
     try {
       const response = await axios.get("/auth/profile");
-      set({ user: response.data, checkingAuth: false });
+      set({ user: response.data.user, checkingAuth: false });
     } catch (error) {
       console.log(error.message);
       set({ checkingAuth: false, user: null });
